dc00884f2b7ae412cebb9cb824cd13c102b912ae
Substitute <Device>
2023-03-23 13:28:14 +0900

    ブーメランある程度挙動完成

--- Hackathon_Spring_2022_No_6/code/boomerang.cpp
+++ Hackathon_Spring_2022_No_6/code/boomerang.cpp
@@ -9,11 +9,13 @@
 #include "boomerang.h"
 
 //マクロ
-#define BOOMERANG_SETUP_NUM		(2)		//ブーメランセットアップ番号
-#define BOOMERANG_ONE_ROTATE	(15)	//ブーメラン自体が1周するのにかかるフレーム数（2PIで割る）
-#define BOOMERANG_THROW_ROT		(0.25f * D3DX_PI)	//ブーメランを投げる角度
-#define BOOMERANG_MOVE_ROT		((1.0f / 60) * D3DX_PI)	//ブーメランの移動の回転速度
-#define BOOMERANG_MOVE_SPEED	(10.0f)	//ブーメランの移動速度
+#define BOOMERANG_SETUP_NUM			(2)		//ブーメランセットアップ番号
+#define BOOMERANG_ONE_ROTATE		(15)	//ブーメラン自体が1周するのにかかるフレーム数（2PIで割る）
+#define BOOMERANG_MOVE_ROT			((1.0f / 60) * D3DX_PI)	//ブーメランの移動の回転速度
+#define BOOMERANG_MOVE_SPEED		(5.0f)	//ブーメランの移動速度
+#define BOOMERANG_ADDROTFORCE		(0.0001f * D3DX_PI)	//回転力増加・減少量
+#define BOOMERANG_DECRETIME_DEF		(40)	//回転力減少までの時間のベース
+#define BOOMERANG_DECRETIME_RAND	(10)	//上の時間の乱数設定部分
 
 #define FIX_ROT(x)				(fmodf(x + (D3DX_PI * 3), D3DX_PI * 2) - D3DX_PI)	//角度を-PI~PIに修正
 
@@ -32,6 +34,9 @@ void InitBoomerang(void)
 		g_aBoomerang[nCntBoomerang].posOld = g_aBoomerang[nCntBoomerang].pos = INITD3DXVECTOR3;
 		g_aBoomerang[nCntBoomerang].move = INITD3DXVECTOR3;
 		g_aBoomerang[nCntBoomerang].rot = INITD3DXVECTOR3;
+		g_aBoomerang[nCntBoomerang].fRotForce = 0.0f;
+		g_aBoomerang[nCntBoomerang].nThrowTime = 0;
+		g_aBoomerang[nCntBoomerang].nDecreTime = 0;
 
 		// 部品(3D)の初期化処理
 		InitParts3DInfo(&g_aBoomerang[nCntBoomerang].partsInfo, BOOMERANG_SETUP_NUM);
@@ -58,6 +63,18 @@ void UpdateBoomerang(void)
 	{
 		if (g_aBoomerang[nCntBoomerang].bUse == true)
 		{
+			//[内部]回転する
+			g_aBoomerang[nCntBoomerang].nThrowTime++;
+			if (g_aBoomerang[nCntBoomerang].nThrowTime < g_aBoomerang[nCntBoomerang].nDecreTime)
+			{
+				g_aBoomerang[nCntBoomerang].fRotForce += BOOMERANG_ADDROTFORCE;
+			}
+			else
+			{
+				g_aBoomerang[nCntBoomerang].fRotForce -= BOOMERANG_ADDROTFORCE;
+			}
+			g_aBoomerang[nCntBoomerang].rot.y = FIX_ROT(g_aBoomerang[nCntBoomerang].rot.y - BOOMERANG_MOVE_ROT - g_aBoomerang[nCntBoomerang].fRotForce);
+
 			//[内部]移動する
 			g_aBoomerang[nCntBoomerang].move.x = -sinf(g_aBoomerang[nCntBoomerang].rot.y) * BOOMERANG_MOVE_SPEED;
 			g_aBoomerang[nCntBoomerang].move.y = 0.0f;
@@ -79,10 +96,19 @@ void UpdateBoomerang(void)
 
 
 			//[見た目]ブーメランの回転処理
-			g_aBoomerang[nCntBoomerang].partsInfo.rot.y = FIX_ROT(g_aBoomerang[nCntBoomerang].rot.y + ((2 * D3DX_PI) / BOOMERANG_ONE_ROTATE));
+			g_aBoomerang[nCntBoomerang].partsInfo.pos = g_aBoomerang[nCntBoomerang].pos;
+			g_aBoomerang[nCntBoomerang].partsInfo.rot.y = FIX_ROT(g_aBoomerang[nCntBoomerang].partsInfo.rot.y + ((2 * D3DX_PI) / BOOMERANG_ONE_ROTATE));
 
-			// 部品(3D)の更新処理
-			UpdateParts3DInfo(&g_aBoomerang[nCntBoomerang].partsInfo);
+			//画面外に出たら消す
+			if (fabs(g_aBoomerang[nCntBoomerang].pos.x) > 250.0f || fabs(g_aBoomerang[nCntBoomerang].pos.z) > 250.0f)
+			{
+				g_aBoomerang[nCntBoomerang].bUse = false;
+			}
+			else
+			{
+				// 部品(3D)の更新処理
+				UpdateParts3DInfo(&g_aBoomerang[nCntBoomerang].partsInfo);
+			}
 		}
 	}
 }
@@ -109,6 +135,8 @@ void SetBoomerang(D3DXVECTOR3 pos, D3DXVECTOR3 rot)
 			g_aBoomerang[nCntBoomerang].posOld = g_aBoomerang[nCntBoomerang].pos = pos;
 			g_aBoomerang[nCntBoomerang].rot = rot;
 			g_aBoomerang[nCntBoomerang].move = INITD3DXVECTOR3;
+			g_aBoomerang[nCntBoomerang].nThrowTime = 0;
+			g_aBoomerang[nCntBoomerang].nDecreTime = (rand() % BOOMERANG_DECRETIME_RAND) + BOOMERANG_DECRETIME_DEF;
 
 			// 部品(3D)の初期化処理
 			InitParts3DInfo(&g_aBoomerang[nCntBoomerang].partsInfo, BOOMERANG_SETUP_NUM);
